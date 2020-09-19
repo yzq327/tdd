55eb60687fb1a2d1e9fed70781b5275899e58237
yzq <you@example.com>
2020-09-19 21:54:59 +0800

    should_return_string_when_vowel_lesser_than_third_of_percentage

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -2,9 +2,15 @@
 public class Mommifier {
 
     public String getNewString(String string){
+        String resultString = string;
+        int strLenth = string.length();
+        int percentage = (int) (strLenth*0.3);
+        String vowels ="aeiou";
+        int count=0;
+        if(string.length()>(vowels.length()/0.3)){
+        }
 
-        int strLenth=string.length();
-        return string;
 
+        return resultString;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -9,10 +9,16 @@ public class MommifierTest {
     void setUp() {
         mommifier = new Mommifier();
     }
+
     @Test
-    void should_return_string_when_without_no_vowels (){
+    void should_return_string_when_without_no_vowels(){
         String string="qqqqq";
         assertEquals("qqqqq", mommifier.getNewString(string));
     }
 
+    @Test
+    void should_return_string_when_vowel_lesser_than_third_of_percentage (){
+        String string="qqqqqqaeiou";
+        assertEquals("qqqqqqaeiou", mommifier.getNewString(string));
+    }
 }
